a4133eab16d4fbfe42bb89e7aca1228d16a12dc4
toqawasfi <toqawasfi123@gmail.com>
2023-03-26 13:48:40 +0000

    cards done

--- src/components/SadaqahStep/SadaqahStep.js
+++ src/components/SadaqahStep/SadaqahStep.js
@@ -3,45 +3,163 @@ import './SadaqahStep.css';
 
 const SadaqahStep = () => {
   return (
-    <div className="sadaqah-cards-container">
-      <div className="sadaqah-card">
-        <div className="sadaqah-card-header" style={{ backgroundColor: '#146C94' }}>
-          <h2>Step 1</h2>
+  <>
+    <div class="container">
+    <div class="services">
+        <div class="face face1">
+            <div class="content">
+                <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTTiqs9ErQFSWsnsJs31ePlD3eFI7y2_0OD6F0rIHuL0nR6ZRgLXI3O7XBCzlg2FXxA0dg&usqp=CAU"></img>
+                <h3>Submitting Donations</h3>
+            </div>
         </div>
-        <div className="sadaqah-card-body">
-          <h3>Choose a cause to support</h3>
-          <p>Research different charities and causes to find one that aligns with your values and beliefs.</p>
+        <div class="face face2">
+            <div class="content">
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas cum cumque minus iste veritatis provident at.</p>
+                   
+            </div>
         </div>
-      </div>
-      <div className="sadaqah-card">
-        <div className="sadaqah-card-header" style={{ backgroundColor: '#19A7CE' }}>
-          <h2>Step 2</h2>
-        </div>
-        <div className="sadaqah-card-body">
-          <h3>Set a budget</h3>
-          <p>Determine how much you can afford to give and set a budget for your Sadaqah donations.</p>
-        </div>
-      </div>
-      <div className="sadaqah-card">
-        <div className="sadaqah-card-header" style={{ backgroundColor: '#146C94' }}>
-          <h2>Step 3</h2>
-        </div>
-        <div className="sadaqah-card-body">
-          <h3>Make a donation</h3>
-          <p>Donate to your chosen charity or cause using a secure and reputable payment method.</p>
-        </div>
-      </div>
-      <div className="sadaqah-card">
-        <div className="sadaqah-card-header" style={{ backgroundColor:  '#19A7CE' }}>
-          <h2>Step 4</h2>
-        </div>
-        <div className="sadaqah-card-body">
-          <h3>Share your good deeds</h3>
-          <p>Encourage others to give by sharing your Sadaqah experience and the impact it has made.</p>
-        </div>
-      </div>
-      
     </div>
+    
+    <div class="services">
+        <div class="face face1">
+            <div class="content">
+                <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTsBFpO2-OMDl8ODXiZyQ4FRlP3RQ9N-FCT3KdwaDftxCBQkLcCcuV5GXeqCPlAI2N2rfc&usqp=CAU"></img>
+                <h3>Volunteer phase</h3>
+            </div>
+        </div>
+        <div class="face face2">
+            <div class="content">
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas cum cumque minus iste veritatis provident at.</p>
+                  
+            </div>
+        </div>
+    </div>
+    <div class="services">
+        <div class="face face1">
+            <div class="content">
+                <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT3oqf73v5npRQJD6odrauQDYvT2a4H-vyGYBx4-N2RE43hCrSsoWfn3BVjzsirh52FlaU&usqp=CAU"></img>
+                <h3>Receving Donation </h3>
+            </div>
+        </div>
+        <div class="face face2">
+            <div class="content">
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas cum cumque minus iste veritatis provident at.</p>
+                   
+            </div>
+        </div>
+    </div>
+    {/* <div class="services">
+        <div class="face face1">
+            <div class="content">
+                <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRKWx7BC4ZNvchUlJYsrNnBRR5idnhpHz2Y5Sg1Ovmig5gXWo-Bm7K1FnC5ucM0CvgG3O4&usqp=CAU"></img>
+                <h3>Moving and packing</h3>
+            </div>
+        </div>
+        <div class="face face2">
+            <div class="content">
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas cum cumque minus iste veritatis provident at.</p>
+                    <a href="#">Read More</a>
+            </div>
+        </div>
+    </div>
+    <div class="services">
+        <div class="face face1">
+            <div class="content">
+                <img src="https://www.in.gov/doe/images/step5.png"></img>
+                <h3>HVAC repair</h3>
+            </div>
+        </div>
+        <div class="face face2">
+            <div class="content">
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas cum cumque minus iste veritatis provident at.</p>
+                    <a href="#">Read More</a>
+            </div>
+        </div>
+    </div> */}
+    {/* <div class="services">
+        <div class="face face1">
+            <div class="content">
+                <img src="https://icon-library.com/images/carpentry-icon/carpentry-icon-15.jpg"></img>
+                <h3>Carpentry</h3>
+            </div>
+        </div>
+        <div class="face face2">
+            <div class="content">
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas cum cumque minus iste veritatis provident at.</p>
+                    <a href="#">Read More</a>
+            </div>
+        </div>
+    </div>
+    <div class="services">
+        <div class="face face1">
+            <div class="content">
+                <img src="https://cdn.discordapp.com/attachments/1062648819801653329/1064140995584872530/Pngtreepaint_brush_line_black_icon_4008027.png"></img>
+                <h3>Painting</h3>
+            </div>
+        </div>
+        <div class="face face2">
+            <div class="content">
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas cum cumque minus iste veritatis provident at.</p>
+                    <a href="#">Read More</a>
+            </div>
+        </div>
+    </div> */}
+    {/* <div class="services">
+        <div class="face face1">
+            <div class="content">
+                <img src="https://github.com/Jhonierpc/WebDevelopment/blob/master/CSS%20Card%20Hover%20Effects/img/launch_128.png?raw=true"></img>
+                <h3>Others</h3>
+            </div>
+        </div>
+        <div class="face face2">
+            <div class="content">
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas cum cumque minus iste veritatis provident at.</p>
+                    <a href="#">Read More</a>
+            </div>
+        </div>
+    </div> */}
+</div>
+  
+</>
+    // <div className="sadaqah-cards-container">
+    //   <div className="sadaqah-card">
+    //     <div className="sadaqah-card-header" style={{ backgroundColor: '#146C94' }}>
+    //       <h2>Step 1</h2>
+    //     </div>
+    //     <div className="sadaqah-card-body">
+    //       <h3>Choose a cause to support</h3>
+    //       <p>Research different charities and causes to find one that aligns with your values and beliefs.</p>
+    //     </div>
+    //   </div>
+    //   <div className="sadaqah-card">
+    //     <div className="sadaqah-card-header" style={{ backgroundColor: '#19A7CE' }}>
+    //       <h2>Step 2</h2>
+    //     </div>
+    //     <div className="sadaqah-card-body">
+    //       <h3>Set a budget</h3>
+    //       <p>Determine how much you can afford to give and set a budget for your Sadaqah donations.</p>
+    //     </div>
+    //   </div>
+    //   <div className="sadaqah-card">
+    //     <div className="sadaqah-card-header" style={{ backgroundColor: '#146C94' }}>
+    //       <h2>Step 3</h2>
+    //     </div>
+    //     <div className="sadaqah-card-body">
+    //       <h3>Make a donation</h3>
+    //       <p>Donate to your chosen charity or cause using a secure and reputable payment method.</p>
+    //     </div>
+    //   </div>
+    //   <div className="sadaqah-card">
+    //     <div className="sadaqah-card-header" style={{ backgroundColor:  '#19A7CE' }}>
+    //       <h2>Step 4</h2>
+    //     </div>
+    //     <div className="sadaqah-card-body">
+    //       <h3>Share your good deeds</h3>
+    //       <p>Encourage others to give by sharing your Sadaqah experience and the impact it has made.</p>
+    //     </div>
+    //   </div>
+      
+    // </div>
   );
 };
 
