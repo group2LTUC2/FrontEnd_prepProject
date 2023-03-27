84da34b3b7da1551cbe5ba6b5342edb7656609a2
Mohammadalsmadi2000 <mohammadalsmadi.fcbayern@gmail.com>
2023-03-27 09:30:42 +0000

    add Donation page

---
+++ src/components/DonationPage/Card.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import './Cards.css'
+function Card({ fullName, item, quantity, location, img, email, user, onDelete, onUpdate, onTake }) {
+  const isOwnedByCurrentUser = user && email === user.email;
+
+  return (
+    <div className="card">
+      <img src={img} alt={item} />
+      <h2>{fullName}</h2>
+      <p>{item}</p>
+      <p>{quantity}</p>
+      <p>{location}</p>
+      {isOwnedByCurrentUser ? (
+        <div>
+          <button onClick={onUpdate}>Update</button>
+          <button onClick={onDelete}>Delete</button>
+        </div>
+      ) : (
+        <button onClick={onTake}>Take</button>
+      )}
+    </div>
+  );
+}
+
+export default Card;
